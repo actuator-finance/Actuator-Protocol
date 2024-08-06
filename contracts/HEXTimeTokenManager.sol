5022c9c7bf74859b76fd5e72c2e17fc0fd2ebc66
RJ <>
2024-08-06 16:26:29 -0500

    use 0.8.24

--- contracts/HEXTimeTokenManager.sol
+++ contracts/HEXTimeTokenManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.25;
+pragma solidity 0.8.24;
 
 // import "hardhat/console.sol";
 import { HEXTimeToken } from "./HEXTimeToken.sol";
--- contracts/interfaces/PulseXFactory.sol
+++ contracts/interfaces/PulseXFactory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.25;
+pragma solidity 0.8.24;
 
 interface IPulseXFactory {
     function getPair(address tokenA, address tokenB) external view returns (address pair);
