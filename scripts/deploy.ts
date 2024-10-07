33465b5dc763355b3890d5af7f9995ec679732bb
RJ <>
2024-10-07 15:42:58 -0500

    more mainnet prep

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,13 +3,11 @@ import * as Const from "./constants";
 import * as Util from "./util";
 
 async function main() {
-  const currentTime = Math.floor(Date.now() / 1000);
-
   const lock = await ethers.deployContract("HEXTimeTokenManager", [
     process.env.TEAM_ADDRESS, 
     process.env.PULSEX_FACTORY_ADDRESS,
     [0n], 
-    currentTime + (60*60 * 24),
+    1728432000,
     Const.FARM_EMISSION_SCHEDULE,
     Const.POOL_POINT_SCHEDULE,
   ]);
